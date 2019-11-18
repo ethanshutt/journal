8ced7c8ad2a651223ab93e8e64a066a5d3c82639
Ethan Shutt <ethan@ethanmadeit>
2019-11-18 05:39:56 -0600

    a working blog almost

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,28 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("src/assets/stylesheets");
     eleventyConfig.addPassthroughCopy("src/assets/scripts");
 
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("DDDD");
+    });
+
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
+
+
     eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+    eleventyConfig.addCollection("posts", function (collection) {
+        return collection.getAllSorted().filter(function (item) {
+            // Only return content that was originally a markdown file
+            // Need to filter better, this will get messy
+            let extension = item.inputPath.split('.').pop();
+            return extension === "md";
+        });
+    });
 
     
     return {
